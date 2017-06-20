72835f8f1d572aa73c17e3d554598bfd2a0d1162
AleSassi <AleSassi@users.noreply.github.com>
2017-06-20 21:32:09 +0200

    Corrected README.md and added commands to Git.swift

--- Git.swift
+++ Git.swift
@@ -1,6 +1,6 @@
 //Git.swift
 //Written by AleSassi on 19/06/17
-//Written on Linux Ubuntu
+//Written on Linux Ubuntu and Xcode 8.3.2/9.0 beta 1
 
 //MARK: This file contains a cheat sheet of the main git commands, listed as plain text or Swift pseudofunctions
 
@@ -15,59 +15,81 @@ git checkout idCommit		Undoes all the changes locally to the specified commit id
 git config --global user.email "your_email@public.com"		Sets up the user public email
 git checkout -b branchName	Creates a new Branch locally
 git push origin branchName	Uploads the commit to the specified branch, either master or the new one
-git push origin branch_name --force
-git fetch origin brachName
-git fetch upstream branchName
-git fetch --all
+git push origin branch_name --force     Pushes the branch online. Use --force to force the command
+git fetch origin brachName              Fetches the branch from the repository. If forked from another project, it fetches from the fork
+git fetch upstream branchName           Fetches the branch from the repository. If forked from another project, it fetches from the main project
+git fetch --all                         Fethes everything from the repository
 git pull
-git checkout -D branchName  Deletes the branchName branch locally
+git checkout -D branchName              Deletes the branchName branch locally
 */
 
 import Foundation
-// Here we have a complete new comment
+
 class GitCommands {
-
-  func pullCommit(){
-    print("git pull")
-  }
-  
-  func pushCommit(fromBranch branch: String){
-    print("git push origin \(branch)")
-  }
-
-  func commit(withMessage commitMessage: String){
-    print("git commit -m \(commitMessage)")
-  }
-  
-  func getStatus() -> String {
-    print("git status")
-    return "This function gets the status of the current Git repo"
-  }
-  
-  func cloneRepo() {
-    print("git clone")
-  }
-  
-  func addFilesToGitRepo(all: Bool) {
-    var addingOption: String!
-    if all{
-      addingOption = "*"
-    } else {
-      addingOption = "."
+    
+    ///"Downloads" a commit from the host service
+    /// - parameters:
+    ///   - commit: The SSH ot https address of the repo to pull
+    func pull(commit: String){
+        print("git pull")
     }
-    print("git add \(addingOption)")
-  }
-  
-  func revert(toCommit idCommit: String, toBranch branch: String){
-    if idCommit != "" {
-      print("git checkout \(idCommit)")
-    } else if idCommit == "" && branch != "" {
-      print("git checkout -b \(branch)")
+    
+    ///"Uploads" a commit from the host service
+    /// - parameters:
+    ///   - branch: The branch name
+    func pushCommit(fromBranch branch: String){
+        print("git push origin \(branch)")
     }
-  }
-
-  func configGit(withUserEmail email: String){
-    print("git config --global user.email \(email)")
-  }
-
+    
+    ///Create a commit with a descriptive message
+    /// - parameters:
+    ///   - commitMessage: The descriptive message of the commit
+    func commit(withMessage commitMessage: String){
+        print("git commit -m \(commitMessage)")
+    }
+    
+    ///Returns the current status of the Git repo
+    func getStatus() -> String {
+        print("git status")
+        return "This function gets the status of the current Git repo"
+    }
+    
+    ///Clones the repository locally
+    /// - parameters:
+    ///   - repo: The SSH ot https address of the repo to pull
+    func clone(repository repo: String) {
+        print("git clone \(repo)")
+    }
+    
+    ///Adds the files to the current Git
+    /// - parameters:
+    ///   - all: Whether to add everything or not (* or .)
+    func addFilesToGitRepo(all: Bool) {
+        var addingOption: String!
+        if all{
+            addingOption = "*"
+        } else {
+            addingOption = "."
+        }
+        print("git add \(addingOption)")
+    }
+    
+    ///Reverts the local project to a previous commit
+    /// - parameters:
+    ///   - idCommit: the commit ID
+    ///   - branch: the branch of the commit
+    func revert(toCommit idCommit: String, toBranch branch: String){
+        if idCommit != "" {
+            print("git checkout \(idCommit)")
+        } else if idCommit == "" && branch != "" {
+            print("git checkout -b \(branch)")
+        }
+    }
+    
+    ///Functions to configure Git on the machine
+    /// - parameters:
+    ///   - email: The user email
+    func configGit(withUserEmail email: String){
+        print("git config --global user.email \(email)")
+    }
 }
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Project1
+# ProjectGit
 ## A small Git cheat sheet
 
-*Project1* is an open-source license-free Git cheat sheet. It contains all the main Git commands to get started with commits, push, pull and branches.
+*ProjectGit* is an open-source license-free Git cheat sheet. It contains all the main Git commands to get started with commits, push, pull and branches.
 
 ### Files
 
 The project contains 2 files:
 **Git.swift**: this is the **main** file. Written as a Swift class, it includes functions linked to Git commands, each of one is printing/returning a String value describing the necessary command to execute in Terminal.
+
 **Test.js**: this .js file just contains some Git commands. Incomplete. For all the commands, see **Git.swift**
