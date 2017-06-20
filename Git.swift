9e2ae73e34bf7f31104a6658532894daee2cce0b
Alessandro Sassi <AleSassi@users.noreply.github.com>
2017-06-20 15:53:41 +0200

    Finishing Swift document
    In this commit, finished to write the text commands as pseudofunctions in Swift

--- Git.swift
+++ Git.swift
@@ -1,3 +1,9 @@
+//Git.swift
+//Written by AleSassi on 19/06/17
+//Written on Linux Ubuntu
+
+//MARK: This file contains a cheat sheet of the main git commands, listed as plain text or Swift pseudofunctions
+
 /*git init
 git pull			Pulls the commit from  online. To specify branch
 git push			Sends the commit online. Branch to be specified
@@ -11,10 +17,51 @@ git checkout -b branchName	Creates a new Branch locally
 git push origin branchName	Uploads the commit to the specified branch, either master or the new one
 */
 
+import Foundation
+
 class GitCommands {
 
 func pullCommit(){
 print("git pull")
+}
+
+func pushCommit(fromBranch branch: String){
+print("git push origin \(branch)")
+}
+
+func commit(withMessage commitMessage: String){
+print("git commit -m \(commitMessage)")
+}
+
+func getStatus() -> String {
+print("git status")
+return "This function gets the status of the current Git repo"
+}
+
+func cloneRepo() {
+print("git clone")
+}
+
+func addFilesToGitRepo(all: Bool) {
+var addingOption: String!
+if all{
+addingOption = "*"
+} else {
+addingOption = "."
+}
+print("git add \(addingOption)")
+}
+
+func revert(toCommit idCommit: String, toBranch branch: String){
+if idCommit != nil{
+print("git checkout \(idCommit)")
+} else if idCommit == nil && branch != nil {
+print("git checkout -b \(branch)"
+}
+}
+
+func configGit(withUserEmail email: String){
+print("git config --global user.email \(email)")
 }
 
 }
